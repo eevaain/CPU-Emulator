e5abbe6e8a1a58cca76272d1f4b0a667e25b2d2a
Evan Lin <evanlin416@gmail.com>
2022-08-17 14:17:36 -0400

    started chip8 class

--- CPU-Emulator/main.cpp
+++ CPU-Emulator/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cstdint>
+
+// curly brackets for key pairs
+
+class Chip8 {
+public:
+	uint8_t registers[16];
+	uint8_t memory[4000];
+	uint16_t index_register{};
+	uint16_t program_counter{}; 
+	uint16_t stack[16];
+
+};
+
+
+// uint8_t
 
 
 int main() {
 
-
+	// add clock loop here
 
 	return 0;
 }
