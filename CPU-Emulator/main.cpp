9d2c08259918be9c830d98992299d605e1fb7d76
eevaain <evanlin416@gmail.com>
2022-08-25 16:10:46 -0400

    adasdass

--- CPU-Emulator/main.cpp
+++ CPU-Emulator/main.cpp
@@ -7,9 +7,14 @@ class Chip8 {
 public:
 	uint8_t registers[16];
 	uint8_t memory[4000];
-	uint16_t index_register{};
-	uint16_t program_counter{}; 
+	uint16_t indexRegister{};
+	uint16_t programCounter{}; 
 	uint16_t stack[16];
+	uint8_t stackPointer{};
+	uint8_t delayTimer{};
+	uint8_t inputKeys[16];
+	uint32_t displayMemory[64 * 32];
+	uint16_t classMembers{};
 
 };
 
